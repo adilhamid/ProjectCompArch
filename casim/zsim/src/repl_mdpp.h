1d09636a76e4ba39adea1008e14d199efd4b9b5f
adilhamid <aaddil.hamid@gmail.com>
2017-12-09 20:21:34 -0600

    Finalized Code

---
+++ casim/zsim/src/repl_mdpp.h
@@ -0,0 +1,242 @@
+/*
+* repl_mddp.h
+*
+*/
+#ifndef REPL_MDPP_H_
+#define REPL_MDPP_H_
+
+#include "repl_policies.h"
+#include<math.h>
+#include<algorithm>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// MDDP Implementation
+class MDPPReplPolicy : public ReplPolicy {
+protected:
+
+	uint32_t* array;
+	uint32_t numLines;
+	vector<uint32_t> candArray;
+	vector<uint32_t> copyList;
+	uint32_t numCands;
+
+
+public:
+	MDPPReplPolicy(uint32_t _numLines, int pNumCands) :
+		numLines(_numLines), numCands(pNumCands)
+	{
+		info("the number of the lines are %d", numLines);
+		array = gm_calloc<uint32_t>(numLines);
+		for (uint32_t i = 0; i < numLines; i++) {
+			array[i] = i;
+		}
+		candArray = vector<uint32_t>(pNumCands);
+		copyList = vector<uint32_t>(pNumCands);
+	}
+
+	~MDPPReplPolicy() {
+		gm_free(array);
+	}
+
+	//Populating the Candidate Array
+	void populateCandArray(int startId) {
+		candArray.clear();
+		for (uint32_t i = 0; i < numCands; i++) {
+			candArray[i] = array[startId + i];
+		}
+	}
+
+	//Populate Back
+	void populateArray(int startId) {
+		for (uint32_t i = 0; i < numCands; i++) {
+			array[startId + i] = candArray[i];
+		}
+	}
+
+	// Get Updated ID
+	uint32_t getUpdatedId(uint32_t id) {
+		for (uint32_t t = 0; t < numCands; t++) {
+			if (candArray[t] == id) {
+				return t;
+			}
+		}
+		panic("I couldn't find the ID");
+	}
+
+	// Table for Lookup of oldIndex to newIndex
+	uint32_t lookUpTable(uint32_t oldIndex) {
+		uint32_t newIndex = oldIndex;
+		switch (oldIndex) {
+		case 15:
+		case 14:
+		case 13:
+		case 12:
+		case 11:
+		case 10:
+		case 9:
+		case 8:
+			newIndex = oldIndex;
+			break;
+		case 7:
+		case 3:
+		case 1:
+		case 0:
+			newIndex = numCands - 1;
+			break;
+		case 6:
+		case 2:
+			newIndex = numCands - 2;
+			break;
+		case 5:
+			newIndex = numCands - 3;
+			break;
+		case 4:
+			newIndex = numCands - 4;
+			break;
+		default:
+			newIndex = 999999999;
+			break;
+		}
+		return newIndex;
+	}
+
+	// Table for Lookup of oldIndex to newIndex
+	uint32_t acessUpdateTable(uint32_t oldIndex) {
+		uint32_t newIndex = oldIndex;
+		switch (oldIndex) {
+		case 7:
+		case 6:
+		case 5:
+		case 4:
+			newIndex = 7;
+			break;
+		case 3:
+		case 2:
+			newIndex = 3;
+			break;
+		case 1:
+		case 0:
+			newIndex = oldIndex;
+			break;
+		default:
+			newIndex = 999999999;
+			break;
+		}
+		return newIndex;
+	}
+
+	void placementUpdate(int start, int end) {
+		for (int i = 0; i < end - start; i++) {
+			int temp = candArray[start + i];
+			candArray[start + i] = candArray[start + i + 1];
+			candArray[start + i + 1] = temp;
+
+		}
+	}
+	void printCandArray() {
+		for (uint32_t cnt = 0; cnt < numCands; cnt++) {
+			info("Val ,%d", candArray[cnt]);
+		}
+	}
+	// Update Code
+	void update(uint32_t id, const MemReq* req) {
+		uint32_t blockId = id / numCands;
+		// Populate the CandArray
+		uint32_t startId = blockId * numCands;
+		populateCandArray(startId);
+
+		if (array[blockId*numCands] == 9999999) { //case 1 : when the block is replaced; MISS
+			array[id] = id;
+			candArray[0] = id;
+			int endLocation = numCands / 4 - 1;
+			placementUpdate(0, endLocation);
+		}
+
+		else {	//case 2 : when the block is updated : HIT
+			uint32_t updatedId = getUpdatedId(id);
+			uint32_t newIndex = lookUpTable(updatedId);
+			if (updatedId != newIndex) {
+				accessUpdate(acessUpdateTable(updatedId));
+			}
+		}
+
+		// Populate the array back after the access update
+		populateArray(blockId*numCands);
+
+	}
+
+	// replaced : check if you want to do the division and multiplication if i am getting not power of 2 as relacement for the ID
+	void replaced(uint32_t id) {
+		uint32_t blockId = id / numCands;
+		array[blockId*numCands] = 9999999; // Marker for the MISS case
+	}
+
+	template <typename C> inline uint32_t rank(const MemReq* req, C cands) {
+		auto first = cands.begin();
+		// Fill candidate Array
+		uint32_t bestCand = array[*first];
+		return bestCand;
+	}
+
+	void accessUpdate(int location) {
+
+		int length = numCands;
+
+		copyList.clear();
+
+		for (int i = 0; i < length; i++)
+		{
+			copyList[i] = candArray[i];
+		}
+		// now access the list in weird way
+		int counterCorrected = 0;
+
+		//Make it most away from being updated
+		counterCorrected++;
+		candArray[length - counterCorrected] = copyList[location];
+
+		//Get the nearest sibling
+		if (location % 2 == 0) { //Left Child
+			counterCorrected++;
+			candArray[length - counterCorrected] = copyList[location + 1];
+
+		}
+		else { //Right Child
+			counterCorrected++;
+			candArray[length - counterCorrected] = copyList[location - 1];
+		}
+
+		// General Case
+		int n = log2(length);
+
+		int lengthReducedSeq = n;
+		int reducedSeq = location;
+		while (counterCorrected < length) {
+			reducedSeq = reducedSeq / 2;
+			lengthReducedSeq--;
+
+			if (reducedSeq % 2 == 0) {
+				reducedSeq++;
+			}
+			else {
+				reducedSeq--;
+			}
+			int newlocations = 0;
+			int numDontCares = n - lengthReducedSeq;
+			int powerDontCares = pow(2, numDontCares);
+			newlocations = powerDontCares * reducedSeq;
+			for (int i = powerDontCares - 1; i >= 0; i--) {
+				counterCorrected++;
+				candArray[length - counterCorrected] = copyList[newlocations + i];
+			}
+		}
+
+	}
+
+	DECL_RANK_BINDINGS;
+};
+
+#endif /* REPL_MDPP_H_ */
--- casim/zsim/src/rrip_repl.h
+++ casim/zsim/src/rrip_repl.h
@@ -3,73 +3,71 @@
 
 #include "repl_policies.h"
 
- // Based on the LRU implementation in the repl_policies.h
-
-// Static RRIP Implementation
-class SRRIPReplPolicy: public ReplPolicy {
-protected:
-	uint32_t RRPVMaxVal; // Represents the MaxValue each entry can have
+// Static RRIP
+class SRRIPReplPolicy : public ReplPolicy {
+    protected:
+        // add class member variables here
+	uint32_t rpvMax;
 	uint64_t* array;
 	uint32_t numLines;
-
-public:
-	SRRIPReplPolicy(uint32_t _numLines, uint32_t _rpvMax) :
-			RRPVMaxVal(_rpvMax), numLines(_numLines){
-		array = gm_calloc<uint64_t>(numLines);
-
-		// Instiantiate the values of each array value as 3
-
-		for( uint32_t id =0 ; id<numLines; id++){
-			array[id] = RRPVMaxVal;
-		}
-	}
-
-	~SRRIPReplPolicy() {
-		gm_free(array);
-	}
-
-	void update(uint32_t id, const MemReq* req) {
-		if (array[id] != 99999) {
-			array[id] = 0; // Represents the hit case, so the value of the array is updated to 0 --> meaning the block is near-reference
-		} else {
-			array[id] = RRPVMaxVal-1; // Represents the replaced case, so the value of the array should be updated to 2 --> meaning the block is long distant-reference
-		}
-	}
-
-	void replaced(uint32_t id) {
-		array[id] = 99999; // marker to let the update function know that this block is miss case i.e replaced
-	}
-
-	template<typename C> inline uint32_t rank(const MemReq* req, C cands) {
-		int bestCand = -1;
-
-		// assumning the C cands represents the array values of the candidates i.e array[id] values in some particular order
-		for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
-			if (array[*ci] == RRPVMaxVal) {
-				bestCand = *ci; // returns the bestCandidate which has the value of 3 in the search
+    public:
+        // add member methods here, refer to repl_policies.h
+		explicit SRRIPReplPolicy(uint32_t _rpvMax, uint32_t _numLines):rpvMax(_rpvMax),numLines(_numLines)
+		{
+			array = gm_calloc<uint64_t>(numLines);
+			for (uint32_t i=0;i<numLines;i++)
+			{
+				array[i]=rpvMax;
 			}
 		}
-
-		while(bestCand == -1) {
-			//Update the RRPV values ( increment by one )
-			for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
-				array[*ci] = (array[*ci] + 1);
-				if(bestCand == -1 && array[*ci] == RRPVMaxVal){ // Get the bestCand which will be the first value updated to 3 from the list of all the candidates
-					bestCand = *ci ;
-				}
+		
+		~SRRIPReplPolicy()
+		{
+			gm_free(array);
+		}
+		
+		void update(uint32_t id,const MemReq* req)
+		{
+			if(array[id]==rpvMax)
+			{
+				array[id]=rpvMax-1;
 			}
-
+			else
+			{
+				array[id]=0;
+			}
 		}
-
-		if(bestCand == -1){
-			std::cout << "Adil: Serious Error happening here"<<std::endl;
+		
+		void replaced(uint32_t id)
+		{
+			array[id]=rpvMax;
 		}
-
-		return (uint32_t)bestCand;
-	}
-
-	DECL_RANK_BINDINGS;
+		
+		template <typename C> inline uint32_t rank(const MemReq* req, C cands) 
+		{
+        	uint32_t bestCand = (uint32_t)-1;
+        	for(auto ci=cands.begin();ci!=cands.end();ci.inc())
+        	{
+        		if(array[*ci]==rpvMax)
+        		{
+        			bestCand=*ci;
+        		}
+        	}
+        	while(bestCand==(uint32_t)-1L)
+        	{
+        		for(auto ci=cands.begin();ci!=cands.end();ci.inc())
+        		{
+        			array[*ci]=array[*ci]+1;
+        			if(array[*ci]==rpvMax)
+        			{
+        				bestCand=*ci;
+        			}
+        		}
+        	}
+        	
+        	return bestCand;
+        	
+        }
+        DECL_RANK_BINDINGS;
 };
-
 #endif // RRIP_REPL_H_
-
